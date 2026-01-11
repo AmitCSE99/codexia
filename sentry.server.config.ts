f494d71aeb4c33db677adc033f6872543f111e69
AmitCSE99 <amitenducse91@gmail.com>
2026-01-11 14:06:20 +0530

    chore: sentry added

---
+++ sentry.server.config.ts
@@ -0,0 +1,24 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ce39a6b5482ae5da3b38d752e8419d8e@o4510226266128384.ingest.us.sentry.io/4510690719825920",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+
+  integrations: [
+    Sentry.vercelAIIntegration,
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+});
--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -2,8 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import * as Sentry from "@sentry/nextjs";
+import { useAuth } from "@clerk/nextjs";
 
 const Page = () => {
+  const { userId } = useAuth();
   const [loading, setLoading] = useState(false);
   const handleBlocking = async () => {
     setLoading(true);
@@ -15,6 +18,22 @@ const Page = () => {
     await fetch("/api/demo/background", { method: "POST" });
     setLoading(false);
   };
+
+  const handleClientError = () => {
+    Sentry.logger.info("User attempting to click on client function", {
+      userId,
+    });
+    throw new Error("Client error: Something went wrong in the browser!");
+  };
+
+  const handleApiError = async () => {
+    await fetch("/api/demo/error", { method: "POST" });
+  };
+
+  const handleInngestError = async () => {
+    await fetch("/api/demo/inngest-error", { method: "POST" });
+  };
+
   return (
     <div className="p-8 space-x-4">
       <Button disabled={loading} onClick={handleBlocking}>
@@ -22,6 +41,15 @@ const Page = () => {
       </Button>
       <Button disabled={loading} onClick={handleBackground}>
         Background
+      </Button>
+      <Button variant="destructive" onClick={handleClientError}>
+        Client Error
+      </Button>
+      <Button variant="destructive" onClick={handleApiError}>
+        API Error
+      </Button>
+      <Button variant="destructive" onClick={handleInngestError}>
+        Inngest Error
       </Button>
     </div>
   );
