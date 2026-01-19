9503c37e8be76979fc43dad05da00e8ed8a43c4e
AmitCSE99 <amitenducse91@gmail.com>
2026-01-19 23:33:02 +0530

    fix: build fix

--- src/app/projects/[projectId]/layout.tsx
+++ src/app/projects/[projectId]/layout.tsx
@@ -7,10 +7,10 @@ const ProjectLayout = async ({
   params,
 }: {
   children: ReactNode;
-  params: Promise<{ projectId: Id<"projects"> }>;
+  params: Promise<{ projectId: string }>;
 }) => {
   const { projectId } = await params;
-  return <ProjectIdLayout projectId={projectId}>{children}</ProjectIdLayout>;
+  return <ProjectIdLayout projectId={projectId as Id<"projects">}>{children}</ProjectIdLayout>;
 };
 
 export default ProjectLayout;
